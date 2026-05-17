37238abe3015676bb1ba3209978a636a3717325c
shivani <shiwani0603@gmail.com>
2026-05-17 12:33:35 +0530

    Remove unused useNavigate

--- src/pages/TelecallerDashboard.js
+++ src/pages/TelecallerDashboard.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { dashboardService, insuranceService, serviceService } from '../services/api';
 import CustomerDetailPanel from '../components/CustomerDetailPanel';
